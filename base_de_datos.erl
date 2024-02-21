ec954951410ee48fb852e43ea4abc8fccf214f1d
AgusMohr <agusmohr01@gmail.com>
2024-02-21 20:06:10 -0300

    commit

--- base_de_datos.erl
+++ base_de_datos.erl
@@ -1,17 +1,26 @@
 -module(base_de_datos).
--export([start/2, stop/0,all/0, remall/0, putt/3, remm/2, gett/1, sizee/0]).
+-export([start/2, stop/0]).
 -import(string, [len/1, concat/2, chr/2, substr/3, str/2, 
 				 to_lower/1, to_upper/1]).
 
 start(Nombre, N) ->
+	Pid = whereis(base_de_datos),
+	if 
+		Pid /= undefined ->
+			unregister(base_de_datos);
+		true ->
+			{true}
+	end,
+	register(base_de_datos, spawn(fun() -> init(Nombre, N) end)).
+
+stop() -> call(stop).
+
+init(Nombre, N) ->
 	Nombre1 = atom_to_list(Nombre),
 	Nombre2 = concat(Nombre1, "-"),
 	Replicas = crear_lista_replicas(Nombre2, N, []),
-	Fun = fun(A) -> replica:start(A, Replicas) end,
-	Replicas1 = lists:map(Fun,Replicas).
-
-stop() -> 
-	stop.
+	start_replics(Replicas, Replicas),
+	loop(Replicas).
 
 crear_lista_replicas(_Nombre,0,List) ->
 	List;
@@ -23,132 +32,34 @@ crear_lista_replicas(Nombre, N, List) ->
 	Nombre2 = [list_to_atom(Nombre1)],
 	List1 = List ++ Nombre2,
 	crear_lista_replicas(Nombre,N2,List1).
-	
-replicas(Replicas) -> 
-	Replicas.
-	
-server(Server) ->
-	receive
-		{consultaServidor, Pid} -> 
-			Pid ! {replyServer, Server},
-			server(Server)
-	end.
 
-loop(Dict) ->
+start_replics([], _Replicas) ->
+	{replics_started};
+
+start_replics([Replica|Tail], Replicas) ->
+	replica:start(Replica, Replicas),
+	start_replics(Tail, Replicas).
+
+loop(Replicas) ->
 	receive
-		{request, Pid, stop} -> 
-			reply(Pid, ok);
-			
-		{request, Pid, all} ->
-			reply(Pid, Dict),
-			loop(Dict);
-		
-		{request,Pid, sizee} ->
-			Size = dict:size(Dict),
-			reply(Pid, Size),
-			loop(Dict);
-			
-		{request, Pid, {gett, Key}} ->
-			Val = dict:find(Key,Dict),
-			if 
-				Val =:= error ->
-					reply(Pid, notfound),
-					loop(Dict);
-				true ->
-					{ok, {Value, TimeStamp, Activo}} = Val,
-					case Activo of
-						true ->
-							reply(Pid, {ok, {Value,TimeStamp}}),
-							loop(Dict);
-						false ->
-							reply(Pid, {ko, TimeStamp}),
-							loop(Dict)
-					end
-				
-			end;
-				
-		{request, Pid, {remm, Key, TimeStamp}} ->
-			Val = dict:find(Key,Dict),
-			if
-				Val =:= error ->
-					Dict1 = dict:store(Key,{null,TimeStamp,false},Dict),
-					reply(Pid, notfound),
-					loop(Dict1);
-				true ->
-					{ok, {_, TimeStamp1, Activo}} = Val,
-					case Activo of
-						true ->
-							if
-								TimeStamp > TimeStamp1 ->
-									Fun = fun({V,_TS, _Activo}) -> {V,TimeStamp,false} end, 
-									Dict1 = dict:update(Key, Fun, Dict),
-									reply(Pid,ok),
-									loop(Dict1);
-								true ->
-									reply(Pid,ko),
-									loop(Dict)
-							end;
-						false ->
-							reply(Pid,notfound),
-							loop(Dict)
-					end
-				
-			end;
-			
-		{request, Pid, remall} ->
-			Keys = dict:fetch_keys(Dict),
-			Dict1 = remov(Keys, Dict),
-			reply(Pid, Dict1),
-			loop(Dict1);
-			
-		{request, Pid, {putt, Key, Value, TimeStamp}} -> 
-			Val = dict:find(Key,Dict),
-			if 
-				Val =:= error ->  
-					Dict1 = dict:store(Key,{Value,TimeStamp,true},Dict),
-					reply(Pid, ok),
-					loop(Dict1);
-				true ->
-					{ok, {_, TimeStamp1, _}} = Val,
-					if 
-						TimeStamp > TimeStamp1 -> 
-							Dict1 = dict:erase(Key,Dict),
-							Dict2 = dict:store(Key,{Value,TimeStamp,true},Dict1),
-							reply(Pid, ok),
-							loop(Dict2);
-						true ->
-							reply(Pid, ko),
-							loop(Dict)
-					end
-			end
+		{request, Pid, stop} ->
+			stop_all(Replicas),
+			reply(Pid, ok)
 	end.
 	
-all() -> call(all).
-remall() -> call(remall).
-putt(Key, Value, TimeStamp) -> call({putt, Key, Value, TimeStamp}).
-remm(Key, TimeStamp) -> call({remm, Key, TimeStamp}).
-gett(Key) -> call({gett, Key}).
-sizee() -> call({sizee}).
+stop_all([]) ->
+	{stop_ok};
+	
+stop_all(Replicas) ->
+	[Replic|Tail] = Replicas,
+	replica:stop(Replic),
+	stop_all(Tail).
 
 call(M) ->
-	servers ! {consultaServidor, self()},
-	receive 
-		{replyServer, Server} -> S = Server
-	end,
-	S ! {request, self(), M},
+	base_de_datos ! {request, self(), M},
 	receive 
 		{reply, Reply} -> Reply
 	end.
 	
 reply(Pid,Reply) ->
 	Pid ! {reply, Reply}.
-
-remov([H|T], Dict) ->
-	Dict2 = dict:erase(H,Dict),
-	remov(T,Dict2);
-
-remov([], Dict) ->
-	Dict = dict:new(),
-	Dict.
-	
-	
