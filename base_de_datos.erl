19454bf5986ee9afd06c0467c3ca6a785351ee71
lautaroba <86685579+lautaroba@users.noreply.github.com>
2024-02-26 19:28:03 -0300

    Add files via upload

--- base_de_datos.erl
+++ base_de_datos.erl
@@ -1,65 +1,46 @@
 -module(base_de_datos).
--export([start/2, stop/0]).
--import(string, [len/1, concat/2, chr/2, substr/3, str/2, 
-				 to_lower/1, to_upper/1]).
+-include_lib("eunit/include/eunit.hrl").
+-export([test/0, test1/0, test2/0, test3/0, test4/0,start/2]).
 
-start(Nombre, N) ->
-	Pid = whereis(base_de_datos),
-	if 
-		Pid /= undefined ->
-			unregister(base_de_datos);
-		true ->
-			{true}
-	end,
-	register(base_de_datos, spawn(fun() -> init(Nombre, N) end)).
+start(Nombre, Cantidad) ->
+    Aux = crear_lista(Nombre, Cantidad),
+    lists:foreach( fun(Elem)-> 
+                            Aux2 = lists:delete(Elem, Aux),
+                            replica_server:start(Elem, Aux2)
+                    end, Aux).
 
-stop() -> call(stop).
+crear_lista(_, 0) -> % Caso base: cuando la cantidad llega a cero, termina la recursión
+    [];
 
-init(Nombre, N) ->
-	Nombre1 = atom_to_list(Nombre),
-	Nombre2 = concat(Nombre1, "-"),
-	Replicas = crear_lista_replicas(Nombre2, N, []),
-	start_replics(Replicas, Replicas),
-	loop(Replicas).
+crear_lista(Nombre, Cantidad) when Cantidad > 0 -> % Caso recursivo: mientras la cantidad sea mayor que cero, sigue construyendo la lista
+    [list_to_atom(atom_to_list(Nombre) ++ integer_to_list(Cantidad)) | crear_lista(Nombre, Cantidad - 1)].
 
-crear_lista_replicas(_Nombre,0,List) ->
-	List;
-	
-crear_lista_replicas(Nombre, N, List) ->
-	N2 = N-1,
-	N1 = integer_to_list(N),
-	Nombre1 = concat(Nombre, N1),
-	Nombre2 = [list_to_atom(Nombre1)],
-	List1 = List ++ Nombre2,
-	crear_lista_replicas(Nombre,N2,List1).
+test() ->
+    start(server, 5).
 
-start_replics([], _Replicas) ->
-	{replics_started};
+%% incerciones de los 3 tipos
+test1() ->
+    replica_server:putt(server1, "1", 1234, os:timestamp(), one),
+    replica_server:putt(server2, "2", 1264, os:timestamp(), quorum),
+    replica_server:putt(server3, "3", 3456, os:timestamp(), all).
 
-start_replics([Replica|Tail], Replicas) ->
-	replica:start(Replica, Replicas),
-	start_replics(Tail, Replicas).
 
-loop(Replicas) ->
-	receive
-		{request, Pid, stop} ->
-			stop_all(Replicas),
-			reply(Pid, ok)
-	end.
-	
-stop_all([]) ->
-	{stop_ok};
-	
-stop_all(Replicas) ->
-	[Replic|Tail] = Replicas,
-	replica:stop(Replic),
-	stop_all(Tail).
+%% busqueda de los 3 tipos
+test2()->    
+    Result1 = replica_server:gett(server1, "1", one),
+    Result2 = replica_server:gett(server3, "2", quorum),
+    Result3 = replica_server:gett(server5, "3", all),
+    {Result1, Result2, Result3}.
 
-call(M) ->
-	base_de_datos ! {request, self(), M},
-	receive 
-		{reply, Reply} -> Reply
-	end.
-	
-reply(Pid,Reply) ->
-	Pid ! {reply, Reply}.
+%% eliminacion de los 3 tipos
+test3() ->
+    replica_server:remm(server1, "1", os:timestamp(), one),
+    replica_server:remm(server4, "15", os:timestamp(), quorum),
+    replica_server:remm(server5, "3", os:timestamp(), all).
+
+%% buscamos las claves anteriores
+test4() ->
+    Result1 = replica_server:gett(server1, "1", one),
+    Result2 = replica_server:gett(server4, "15", quorum),
+    Result3 = replica_server:gett(server5, "3", all),
+    {Result1, Result2, Result3}.
--- replica_server.erl
+++ replica_server.erl
@@ -1,10 +1,10 @@
 -module(replica_server).
--behaviour(gen_server).
 -export([start/2, stop/1, putt/5, remm/4, gett/3, sizee/1, test/0]).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
--record(state, {dict, replicas, pending_puts_all, pending_puts_quorum, pending_rems_all, pending_rems_quorum, pending_gets_all, pending_gets_quorum}).
+-export([init/1, handle_call/3]).
 
-%%% Public API %%%
+-record(state, {dict, replicas, actual_state}).
+
+%%% Funciones publicas %%%
 test() ->
     gen_server:start_link({local, server1}, ?MODULE, [server2, server3], []),
     gen_server:start_link({local, server2}, ?MODULE, [server1, server3], []),
@@ -28,55 +28,126 @@ gett(Name, Key, Consistency) ->
 sizee(Name) ->
     gen_server:call(Name, sizee).
 
-%%% GenServer callbacks %%%
+%%% Funciones privadas %%%
 init(Replicas) ->
-    {ok, #state{dict = dict:store("1", {10, os:timestamp(), true}, dict:new()), replicas = Replicas, 
-    pending_puts_all = [], 
-    pending_puts_quorum = [], 
-    pending_rems_all = [], 
-    pending_rems_quorum = [], 
-    pending_gets_all = [], 
-    pending_gets_quorum = []}}.
+    {ok, #state{dict = dict:new(), replicas = Replicas, actual_state = "free"}}.
 
 handle_call({putt, Key, Value, TimeStamp, Consistency}, _From, State) ->
-    NewState = do_putt(Key, Value, TimeStamp, Consistency, State),
+    {_, NewState} = case Consistency of
+        one ->
+            do_putt_one(Key, Value, TimeStamp, State);
+        quorum ->
+            do_putt_quorum(Key, Value, TimeStamp, State);
+        all ->
+            do_putt_all(Key, Value, TimeStamp, State)
+    end,
     {reply, ok, NewState};
 
 handle_call({remm, Key, TimeStamp, Consistency}, _From, State) ->
-    NewState = do_remm(Key, TimeStamp, Consistency, State),
+    {_, NewState} = case Consistency of
+        one ->
+            do_remm_one(Key, TimeStamp, State);
+        quorum ->
+            do_remm_quorum(Key, TimeStamp, State);
+        all ->
+            do_remm_all(Key, TimeStamp, State)
+    end,
     {reply, ok, NewState};
 
 handle_call({gett, Key, Consistency}, _From, State) ->
-    {reply, do_gett(Key, Consistency, State), State};
+    Val = do_gett(Key, Consistency, State),
+    {reply, Val, State};
 
 handle_call(sizee, _From, State) ->
     {reply, dict:size(State#state.dict), State};
 
 handle_call(stop, _From, State) ->
-    {stop, normal, ok, State}.
+    {stop, normal, ok, State};
 
-handle_cast({store_tuple, Key, Value, TimeStamp}, State) ->
-    % Almacena la tupla en el diccionario
-    case dict:find(Key, State#state.dict) of
-        {ok, {_, ExistingTimeStamp, false}} when ExistingTimeStamp =< TimeStamp ->
-            % Si existe y está activa, actualiza el timestamp al actual y desactiva la tupla
-            NewDict = dict:update(Key, fun({_Value, _ExistingTimeStamp, _IsActive}) -> {Value, TimeStamp, true} end, State#state.dict),
-            {noreply, State#state{dict=NewDict}};
-        {error, _} ->
-            % Si no existe, crea una nueva tupla con valor null, el timestamp actual y desactivada
+handle_call(_Msg , _From, _State ) ->
+    {noreply, error}.
+%%% Internal functions %%%
+do_gett(Key, Consistency, State) ->
+    case Consistency of
+        one ->
+            do_gett_one(Key, State);
+        quorum ->
+            do_gett_quorum(Key, State);
+        all ->
+            do_gett_all(Key, State)
+    end.
+
+do_putt_one(Key, Value, TimeStamp, State) ->
+    Val = dict:find(Key, State#state.dict),
+    case Val of
+        error -> 
             NewDict = dict:store(Key, {Value, TimeStamp, true}, State#state.dict),
             {noreply, State#state{dict=NewDict}};
-        _ ->
-            % Si existe pero está desactivada, no se realiza ninguna acción
-            {noreply, State}
-    end;
+        {_, {_Value1, _TimeStamp1, false}} ->
+            NewDict = dict:update(Key, {Value, TimeStamp, true}, State#state.dict),
+            {noreply, State#state{dict=NewDict}};
+        _ -> {error, State}
+    end.
 
-handle_cast({remove_tuple, Key, TimeStamp}, State) ->
-    % Verifica si la tupla existe y está activa
+do_putt_quorum(Key, Value, TimeStamp, State) ->
+    {Confirmed, _} = lists:foldl(
+        fun(Replica, {Acc, Rejected}) ->
+            case do_check_putt(Replica, Key, TimeStamp, State) of
+                {accept, _} ->
+                    {Acc + 1, Rejected};
+                {reject, _} ->
+                    {Acc, Rejected + 1}
+            end
+        end,
+        {0, 0},
+        State#state.replicas
+    ),
+    if
+        Confirmed >= length(State#state.replicas) div 2 + 1 ->
+            NewDict = dict:store(Key, {Value, TimeStamp, true}, State#state.dict),
+            lists:map(
+                fun(Replica) ->
+                    gen_server:call(Replica, {putt, Key, Value, TimeStamp, one})
+                end,
+                State#state.replicas
+            ),
+            {noreply, State#state{dict=NewDict}};
+        true ->
+            {noreply, State}
+    end.
+
+do_putt_all(Key, Value, TimeStamp, State) ->
+    {Confirmed, _Rejected} = lists:foldl(
+        fun(Replica, {Acc, Rejected}) ->
+            case do_check_putt(Replica, Key, TimeStamp, State) of
+                {accept, _} ->
+                    {Acc + 1, Rejected};
+                {reject, _} ->
+                    {Acc, Rejected + 1}
+            end
+        end,
+        {0, 0},
+        State#state.replicas
+    ),
+    if
+        Confirmed == length(State#state.replicas) ->
+            NewDict = dict:store(Key, {Value, TimeStamp, true}, State#state.dict),
+            lists:map(
+                fun(Replica) ->
+                    gen_server:call(Replica, {putt, Key, Value, TimeStamp, one})
+                end,
+                State#state.replicas
+            ),
+            {noreply, State#state{dict=NewDict}};
+        true ->
+            {noreply, State}
+    end.
+
+do_remm_one(Key, TimeStamp, State) ->
     case dict:find(Key, State#state.dict) of
         {ok, {_, ExistingTimeStamp, true}} when ExistingTimeStamp =< TimeStamp ->
             % Si existe y está activa, actualiza el timestamp al actual y desactiva la tupla
-            NewDict = dict:update(Key, fun({_Value, _ExistingTimeStamp, _IsActive}) -> {null, TimeStamp, false} end, State#state.dict),
+            NewDict = dict:update(Key, fun({Value, _ExistingTimeStamp, _IsActive}) -> {Value, TimeStamp, false} end, State#state.dict),
             {noreply, State#state{dict=NewDict}};
         {error, _} ->
             % Si no existe, crea una nueva tupla con valor null, el timestamp actual y desactivada
@@ -87,129 +158,171 @@ handle_cast({remove_tuple, Key, TimeStamp}, State) ->
             {noreply, State}
     end.
 
-handle_info({putt_checks_completed_all, CanStore}, State) ->
-    lists:foreach(fun({Pid, Key, Value, TimeStamp}) ->
-                Pid ! {store_tuple, Key, Value, TimeStamp}
-            end, State#state.pending_puts_all),
-    {noreply, State#state{pending_puts_all=[]}};
-
-handle_info({putt_checks_completed_quorum, CanStore}, State) ->
-    % Si al menos la mitad de las réplicas pueden almacenar la tupla, se procede a almacenarla
-    Quorum = length(State#state.replicas) div 2 + 1,
-    ConfirmationCount = lists:count(fun({_Pid, true}) -> true; (_) -> false end, CanStore),
-    if
-        ConfirmationCount >= Quorum ->
-            lists:foreach(fun({Pid, Key, Value, TimeStamp}) ->
-                Pid ! {store_tuple, Key, Value, TimeStamp}
-            end, State#state.pending_puts_quorum),
-            {noreply, State#state{pending_puts_quorum=[]}};
-        true ->
-            {noreply, State#state{pending_puts_quorum=[]}}
-    end;
-
-handle_info({remm_checks_completed_all, CanRemove}, State) ->
-    lists:foreach(fun({Pid, Key, TimeStamp}) ->
-                Pid ! {remove_tuple, Key, TimeStamp}
-            end, State#state.pending_rems_all),
-    {noreply, State#state{pending_rems_all=[]}};
-
-handle_info({remm_checks_completed_quorum, CanRemove}, State) ->
-    % Si al menos la mitad de las réplicas pueden eliminar la tupla, se procede a eliminarla
-    Quorum = length(State#state.replicas) div 2 + 1,
-    ConfirmationCount = lists:count(fun({_Pid, true}) -> true; (_) -> false end, CanRemove),
-    if
-        ConfirmationCount >= Quorum ->
-            lists:foreach(fun({Pid, Key, TimeStamp}) ->
-                Pid ! {remove_tuple, Key, TimeStamp}
-            end, State#state.pending_rems_quorum),
-            {noreply, State#state{pending_rems_quorum=[]}};
-        true ->
-            {noreply, State#state{pending_rems_quorum=[]}}
-    end;
-
-handle_info({gett_checks_completed_all, Results}, State) ->
-    Values = [Value || {_, Value} <- Results],
-    case lists:member(not_found, Values) of
-        true ->
-            % Si al menos una réplica no pudo encontrar la tupla, se responde con "not_found"
-            lists:foreach(fun({Pid, _Key}) -> Pid ! {reply, not_found} end, State#state.pending_gets_all);
-        false ->
-            % Si todas las réplicas encontraron la tupla, se responde con el valor de la tupla
-            lists:foreach(fun({Pid, Key}) -> Pid ! {reply, {ok, dict:fetch(Key, State#state.dict)}} end, State#state.pending_gets_all)
-    end,
-    {noreply, State#state{pending_gets_all=[]}};
-
-handle_info({gett_checks_completed_quorum, Results}, State) ->
-    Values = [Value || {_, Value} <- Results],
-    case lists:member(not_found, Values) of
-        true ->
-            % Si al menos una réplica no pudo encontrar la tupla, se responde con "not_found"
-            lists:foreach(fun({Pid, _Key}) -> Pid ! {reply, not_found} end, State#state.pending_gets_quorum);
-        false ->
-            % Si al menos la mitad de las réplicas encontraron la tupla, se responde con el valor de la tupla
-            Quorum = length(State#state.replicas) div 2 + 1,
-            ReplyCount = length(Values),
-            case ReplyCount >= Quorum of
-                true ->
-                    lists:foreach(fun({Pid, Key}) -> Pid ! {reply, {ok, dict:fetch(Key, State#state.dict)}} end, State#state.pending_gets_quorum);
-                false ->
-                    lists:foreach(fun({Pid, _Key}) -> Pid ! {reply, not_found} end, State#state.pending_gets_quorum)
+do_remm_quorum(Key, TimeStamp, State) ->
+    {Confirmed, _} = lists:foldl(
+        fun(Replica, {Acc, Rejected}) ->
+            case do_check_remm(Replica, Key, TimeStamp, State) of
+                {accept, _} ->
+                    {Acc + 1, Rejected};
+                {reject, _} ->
+                    {Acc, Rejected + 1}
             end
-    end,
-    {noreply, State#state{pending_gets_quorum=[]}}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%% Internal functions %%%
-do_putt(Key, Value, TimeStamp, Consistency, State) ->
-    case Consistency of
-        one ->
-            NewDict = dict:store(Key, {Value, TimeStamp, true}, State#state.dict),
-            State#state{dict=NewDict};
-        quorum ->
-            % Envía una solicitud a las réplicas para verificar si la tupla ya existe
-            {noreply, State#state{pending_puts_quorum=[{self(), Key, Value, TimeStamp}|State#state.pending_puts_quorum]}};
-        all ->
-            % Envía una solicitud a todas las réplicas para verificar si la tupla ya existe
-            {noreply, State#state{pending_puts_all=[{self(), Key, Value, TimeStamp}|State#state.pending_puts_all]}}
+        end,
+        {0, 0},
+        State#state.replicas
+    ),
+    if
+        Confirmed >= length(State#state.replicas) div 2 + 1 ->
+            lists:map(
+                fun(Replica) ->
+                    gen_server:call(Replica, {remm, Key, TimeStamp, one})
+                end,
+                State#state.replicas
+            ),
+            case dict:find(Key, State#state.dict) of
+                {ok, {_, ExistingTimeStamp, true}} when ExistingTimeStamp =< TimeStamp ->
+                    % Si existe y está activa, actualiza el timestamp al actual y desactiva la tupla
+                    NewDict = dict:update(Key, fun({Value, _ExistingTimeStamp, _IsActive}) -> {Value, TimeStamp, false} end, State#state.dict),
+                    {noreply, State#state{dict=NewDict}};
+                {error, _} ->
+                    % Si no existe, crea una nueva tupla con valor null, el timestamp actual y desactivada
+                    NewDict = dict:store(Key, {null, TimeStamp, false}, State#state.dict),
+                    {noreply, State#state{dict=NewDict}};
+                _ ->
+                    % Si existe pero está desactivada, no se realiza ninguna acción
+                    {noreply, State}
+            end;
+        true ->
+            {noreply, State}
     end.
 
-do_remm(Key, TimeStamp, Consistency, State) ->
-    case Consistency of
-        one ->
+do_remm_all(Key, TimeStamp, State) ->
+    {Confirmed, _Rejected} = lists:foldl(
+        fun(Replica, {Acc, Rejected}) ->
+            case do_check_remm(Replica, Key, TimeStamp, State) of
+                {accept, _} ->
+                    {Acc + 1, Rejected};
+                {reject, _} ->
+                    {Acc, Rejected + 1}
+            end
+        end,
+        {0, 0},
+        State#state.replicas
+    ),
+    if
+        Confirmed == length(State#state.replicas) ->
+            lists:map(
+                fun(Replica) ->
+                    gen_server:call(Replica, {remm, Key, TimeStamp, one})
+                end,
+                State#state.replicas
+            ),
+            case dict:find(Key, State#state.dict) of
+                {ok, {_, ExistingTimeStamp, true}} when ExistingTimeStamp =< TimeStamp ->
+                    % Si existe y está activa, actualiza el timestamp al actual y desactiva la tupla
+                    NewDict = dict:update(Key, fun({_Value, _ExistingTimeStamp, _IsActive}) -> {null, TimeStamp, false} end, State#state.dict),
+                    {noreply, State#state{dict=NewDict}};
+                {error, _} ->
+                    % Si no existe, crea una nueva tupla con valor null, el timestamp actual y desactivada
+                    NewDict = dict:store(Key, {null, TimeStamp, false}, State#state.dict),
+                    {noreply, State#state{dict=NewDict}};
+                _ ->
+                    % Si existe pero está desactivada, no se realiza ninguna acción
+                    {noreply, State}
+            end;
+        true ->
+            {noreply, State}
+    end.
+
+do_gett_one(Key, State) ->
+    Val = dict:find(Key, State#state.dict),
+    case Val of
+        {_, {Value1, TimeStamp1, true}} -> {Value1, TimeStamp1};
+        error -> notfound;
+        _ -> notfound
+    end.
+
+do_gett_quorum(Key, State) ->
+    Replies = lists:map(
+        fun(Replica) ->
+            case do_gett_one_replica(Replica, Key, State) of
+                notfound -> notfound;
+               {Value1, TimeStamp1} -> {Value1, TimeStamp1}
+            end
+        end,
+        State#state.replicas
+    ),
+    RepliesFiltered = lists:filter(
+        fun({_Value, _TimeStamp}) -> true;
+           (_) -> false
+        end,
+        Replies
+    ),
+    case RepliesFiltered of
+        [] -> empty;
+        _ -> lists:max(RepliesFiltered)
+    end.
+
+do_gett_all(Key, State) ->
+    Replies = lists:map(
+        fun(Replica) ->
+            do_gett_one_replica(Replica, Key, State)
+        end,
+        State#state.replicas
+    ),
+    case lists:any(fun(X) -> X == notfound end, Replies) of
+        true -> notfound;
+        false ->
+            RepliesFiltered = lists:filter(
+                fun({_Value, _TimeStamp}) -> true;
+                   (_) -> false
+                end,
+                Replies
+            ),
+            case RepliesFiltered of
+                [] -> empty;
+                _ -> lists:max(RepliesFiltered)
+            end
+    end.
+
+do_gett_one_replica(Replica, Key, State) ->
+    case State#state.actual_state of
+        "free" ->
+            case gen_server:call(Replica, {gett, Key, one}) of
+                {Value1, Reply} -> {Value1, Reply};
+                _ -> notfound
+            end;
+        "occupied" ->
+            notfound
+    end.
+
+do_check_putt(Replica, Key, TimeStamp, State) ->
+    case State#state.actual_state of
+        "free" ->
+            case dict:find(Key, State#state.dict) of
+                {ok, {_, ExistingTimeStamp, true}} when ExistingTimeStamp =< TimeStamp ->
+                    {reject, Replica};
+                _ ->
+                    {accept, Replica}
+            end;
+        "occupied" ->
+            {reject, Replica}
+    end.
+
+do_check_remm(Replica, Key, TimeStamp, State) ->
+    case State#state.actual_state of
+        "free" ->
                 case dict:find(Key, State#state.dict) of
                     {ok, {_, ExistingTimeStamp, true}} when ExistingTimeStamp =< TimeStamp ->
                         % Si existe y está activa, actualiza el timestamp al actual y desactiva la tupla
-                        NewDict = dict:update(Key, fun({_Value, _ExistingTimeStamp, _IsActive}) -> {null, TimeStamp, false} end, State#state.dict),
-                        {noreply, State#state{dict=NewDict}};
+                        {accept, Replica};
                     {error, _} ->
                         % Si no existe, crea una nueva tupla con valor null, el timestamp actual y desactivada
-                        NewDict = dict:store(Key, {null, TimeStamp, false}, State#state.dict),
-                        {noreply, State#state{dict=NewDict}};
+                        {accept, Replica};
                     _ ->
                         % Si existe pero está desactivada, no se realiza ninguna acción
-                        {noreply, State}
-                end.
-        quorum ->
-            % Envía una solicitud a las réplicas para verificar si la tupla ya existe
-            {noreply, State#state{pending_rems_quorum=[{self(), Key, TimeStamp} | State#state.pending_rems_quorum]}};
-        all ->
-            % Envía una solicitud a todas las réplicas para verificar si la tupla ya existe
-            {noreply, State#state{pending_rems_all=[{self(), Key, TimeStamp} | State#state.pending_rems_all]}}
-    end.
-
-do_gett(Key, Consistency, State) ->
-    case Consistency of
-        one ->
-            dict:find(Key, State#state.dict);
-        quorum ->
-            % Envía una solicitud a las réplicas para obtener el valor de la tupla
-            {noreply, State#state{pending_gets_quorum=[{self(), Key} | State#state.pending_gets_quorum]}};
-        all ->
-            % Envía una solicitud a todas las réplicas para obtener el valor de la tupla
-            {noreply, State#state{pending_gets_all=[{self(), Key} | State#state.pending_gets_all]}}
+                        {reject, Replica}
+                end;
+        "occupied" ->
+            {reject, Replica}
     end.
